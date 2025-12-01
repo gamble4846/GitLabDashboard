f1ad081455a3c413ff2f2933151d959ee4bdbbb6
rohanpatelsatva <rohanp@satvasolutions.com>
2025-12-01 17:31:49 +0530

    no message

--- GitLabDashboard/src/app/components/header/header.component.ts
+++ GitLabDashboard/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
@@ -11,5 +11,14 @@ import { AuthService } from '../../services/auth.service';
 })
 export class HeaderComponent {
   authService = inject(AuthService);
+  mobileMenuOpen = signal(false);
+
+  toggleMobileMenu(): void {
+    this.mobileMenuOpen.update(value => !value);
+  }
+
+  closeMobileMenu(): void {
+    this.mobileMenuOpen.set(false);
+  }
 }
 
